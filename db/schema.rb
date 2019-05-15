086bd9e576c890f2fe69a94efb2542b69aa2ca8e
Alexander Moskovskiy <aleksandrmoskovskiy@a-mosk-2.local>
2019-05-16 01:23:50 +0300

    Story in Comics. Stable

--- db/schema.rb
+++ db/schema.rb
@@ -28,6 +28,14 @@ ActiveRecord::Schema.define(version: 2019_05_14_091140) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "stories", force: :cascade do |t|
+    t.string "title"
+    t.string "adress"
+    t.integer "comic_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
