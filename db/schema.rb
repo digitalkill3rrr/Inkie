ec5b5f38c7ec70b88c170f9cb47bed05ee09861b
digitalkill3rrr <mkiller.shinoda@gmail.com>
2019-06-23 20:18:12 +0300

    test deploy

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_06_06_152222) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "comics", force: :cascade do |t|
     t.string "title"
     t.string "cover"
